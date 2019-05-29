71419e0d66bab067ecf0cb79c686cae97777f61c
sg <srfngrc@outlook.com>
2019-05-28 20:18:16 -0400

    finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.lsvTable = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Table to run from o through: ";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(25, 64);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(144, 64);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(174, 27);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(45, 20);
+            this.txtMax.TabIndex = 3;
+            // 
+            // lsvTable
+            // 
+            this.lsvTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvTable.Location = new System.Drawing.Point(25, 109);
+            this.lsvTable.Name = "lsvTable";
+            this.lsvTable.Size = new System.Drawing.Size(194, 229);
+            this.lsvTable.TabIndex = 4;
+            this.lsvTable.UseCompatibleStateImageBehavior = false;
+            this.lsvTable.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "N";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "N*N";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "N*N*N";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 409);
+            this.ClientSize = new System.Drawing.Size(252, 367);
+            this.Controls.Add(this.lsvTable);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Table of numbers";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.ListView lsvTable;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,44 @@ namespace Chaper08ProgramTwoDimensionalArray
         {
             InitializeComponent();
         }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int number;
+            int i;
+            ListViewItem which;
+            flag = int.TryParse(txtMax.Text, out number); // check input
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric data only.", "Input Error");
+                txtMax.Focus();
+                return;
+            }
+            if (number < 0) // Make sure it's positive
+            {
+                number = number * 1;
+            }
+            number++; // Do this because of N - 1 Rule
+            int[,] myData = new int[number, 3]; // Define array
+            for (i = 0; i < number; i++)
+            {
+                myData[i, 0] = i; // first column of table
+                myData[i, 1] = i * i; // second column of table
+                myData[i, 2] = i * i * i; // third column of table
+            }
+            for (i = 0; i < number; i++) // Now show it
+            {
+                which = new ListViewItem(myData[i, 0].ToString());
+                which.SubItems.Add(myData[i, 1].ToString());
+                which.SubItems.Add(myData[i, 2].ToString());
+                lsvTable.Items.Add(which);
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
